7c5cac1873b9691a8f6ac56cab051dad379c5a16
Wilco de Boer <deboer.wilco@gmail.com>
2024-01-15 22:30:09 +0100

    Update `tellme` processing script

--- extras/ores.py
+++ extras/ores.py
@@ -1,11 +1,11 @@
 with open('ores.txt', 'w') as out:
-    with open('../config/tellme/blocks_2022-08-18_17.08.35.txt', 'r') as inp:
+    with open('../config/tellme/blocks_2023-06-07_16.45.30.txt', 'r') as inp:
         for line in inp:
             if '|' in line and line.split('|')[7].strip().endswith(' Ore'):
                 out.write(line)
 
 with open('coins.txt', 'w') as out:
-    with open('../config/tellme/items_2022-08-18_17.08.40.txt', 'r') as inp:
+    with open('../config/tellme/items_2023-06-07_16.45.35.txt', 'r') as inp:
         for line in inp:
-            if ' Coin ' in line or ' Stamp ' in line:
+            if ' Coin ' in line or ' Stamp ' in line or ' Experience ' in line:
                 out.write(line)
